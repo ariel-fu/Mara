8ae2d0a075ce5377197013b8586b9e5df59d0536
Ariel Fu <ariel.fu@gmail.com>
2024-01-23 02:36:33 -0500

    launch screen page

---
+++ demo_app/lib/launch_screen.dart
@@ -0,0 +1,36 @@
+// launch_screen.dart
+
+import 'package:flutter/material.dart';
+import 'main.dart'; // Import your main.dart file
+
+class LaunchScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Cat Care',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyHomePage()),
+                );
+              },
+              child: Text('Get Started 😺'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
