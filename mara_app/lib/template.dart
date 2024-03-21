5745fdcb10e0b4bcae69cbe5b18b24e3968736d5
ariel <ariel.fu@gmail.com>
2024-03-20 18:49:48 -0700

    add template and time

--- mara_app/lib/template.dart
+++ mara_app/lib/template.dart
@@ -10,26 +10,48 @@ class TemplatePage extends StatefulWidget {
 
 class _TemplatePageState extends State<TemplatePage> {
   Widget methodContent = Text('DUMMY');
-  // TODO - replace the language/content with a hashmap?
-  int methodIndex = -1; // Index of the selected icon button, -1 for none
-  int languageIndex = -1; // similar indexing for language
+  int methodIndex = 0; // Index of the selected icon button, 0 for default
+  int languageIndex = 0; // similar indexing for language
   final languages = ["Kiswahili", "Dholuo", "English"];
-  final content = [
-    //TODO: one of these content Strings could be a videoWidget
-    //but the asset passed into the videoWidget would vary based 
-    //on language and method so we'd need a HashMap to manage that
-    "method 1",
-    "method 2",
-    "method 3",
-    "method 4",
-    "method 5",
-    "method 6"
-  ];
+
+  final Map<String, List<String>> contentDescriptionMap = {
+    "Kiswahili": [
+      "method 1 in Kiswahili",
+      "method 2 in Kiswahili",
+      "method 3 in Kiswahili",
+      "method 4 in Kiswahili",
+      "method 5 in Kiswahili",
+      "method 6 in Kiswahili",
+      "method 7 in Kiswahili"
+    ],
+    "Dholuo": [
+      "method 1 in Dholuo",
+      "method 2 in Dholuo",
+      "method 3 in Dholuo",
+      "method 4 in Dholuo",
+      "method 5 in Dholuo",
+      "method 6 in Dholuo",
+      "method 7 in Dholuo"
+    ],
+    "English": [
+      "method 1 in English",
+      "method 2 in English",
+      "method 3 in English",
+      "method 4 in English",
+      "method 5 in English",
+      "method 6 in English",
+      "method 7 in English"
+    ],
+  };
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double boxWidth = screenWidth * 0.85;
-    double boxHeight = boxWidth * 0.5;
+
+    double screenHeight = MediaQuery.of(context).size.height;
+    double availableHeight = screenHeight;
+    double boxHeight = availableHeight * 0.25;
 
     return Scaffold(
       appBar: AppBar(
@@ -39,99 +61,117 @@ class _TemplatePageState extends State<TemplatePage> {
             Navigator.of(context).pushNamed('/home');
           },
         ),
-        title: Text('ANY TITLE HERE!'),
+        title: Text('Can I keep it private?'),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
-            padding: EdgeInsets.symmetric(vertical: 8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      languageIndex = 0;
-                      updateMethodContent();
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: languageIndex == 0 ? Colors.grey : null,
-                  ),
-                  child: Text('Kiswahili'),
+              height: availableHeight * 0.1,
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          languageIndex = 0;
+                          updateMethodContent();
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            languageIndex == 0 ? Colors.grey : null,
+                      ),
+                      child: Text('Kiswahili'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          languageIndex = 1;
+                          updateMethodContent();
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            languageIndex == 1 ? Colors.grey : null,
+                      ),
+                      child: Text('Dholuo'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          languageIndex = 2;
+                          updateMethodContent();
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            languageIndex == 2 ? Colors.grey : null,
+                      ),
+                      child: Text('English'),
+                    ),
+                  ],
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      languageIndex = 1;
-                      updateMethodContent();
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: languageIndex == 1 ? Colors.grey : null,
-                  ),
-                  child: Text('Dholuo'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      languageIndex = 2;
-                      updateMethodContent();
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: languageIndex == 2 ? Colors.grey : null,
-                  ),
-                  child: Text('English'),
-                ),
-              ],
+              )),
+          SizedBox(height: 20.0),
+          Container(
+            alignment: Alignment.center,
+            height: availableHeight * 0.1,
+            width: boxWidth,
+            // padding: EdgeInsets.symmetric(horizontal: 0.1*boxWidth),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildIconButton(MaraIcons.condom, 0),
+                  buildIconButton(MaraIcons.female_condom, 1),
+                  buildIconButton(MaraIcons.birth_control_pills, 2),
+                  buildIconButton(MaraIcons.syringe, 3),
+                  buildIconButton(MaraIcons.contraceptive_implant, 4),
+                  buildIconButton(MaraIcons.iud, 5),
+                  buildIconButton(MaraIcons.double_pills, 6),
+                  
+                ],
+              ),
             ),
           ),
           SizedBox(height: 20.0),
           Container(
-            padding: EdgeInsets.symmetric(horizontal: 50.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+            height: availableHeight * 0.6, // Adjust as needed
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                buildIconButton(MaraIcons.iud, 0),
-                buildIconButton(MaraIcons.birth_control_pills, 1),
-                buildIconButton(MaraIcons.condom, 2),
-                buildIconButton(
-                    MaraIcons.contraceptive_implant, 3),
-                buildIconButton(MaraIcons.syringe, 4),
-                buildIconButton(MaraIcons.female_condom, 5),
+                Container(
+                  width: boxWidth,
+                  height: boxHeight,
+                  color: Colors.blue,
+                  child: Center(
+                    child: methodContent,
+                  ),
+                ),
+                // SizedBox(height: 10.0),
+                Container(
+                  width: boxWidth * 0.75,
+                  height: availableHeight * 0.25 - 10,
+                  color: Colors.green,
+                  child: Center(
+                    child: buildSecondaryContext(),
+                  ),
+                ),
               ],
             ),
-          ),
-          SizedBox(height: 16.0),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                width: boxWidth,
-                height: boxHeight,
-                color: Colors.blue,
-                child: Center(
-                  // TODO - this methodContent is the first text/video/audio content associated with the method
-                  child: methodContent,
-                ),
-              ),
-              SizedBox(height: 20.0),
-              Container(
-                width: boxWidth,
-                height: boxHeight,
-                color: Colors.green,
-                child: Center(
-                  // TODO - this methodContent is the second text/video/audio content associated with the method
-                  child: methodContent,
-                ),
-              ),
-            ],
           ),
         ],
       ),
     );
+  }
+
+  Widget buildSecondaryContext() {
+    return Text("some text here " + 
+      contentDescriptionMap[languages[languageIndex]]![methodIndex]);
   }
 
   Widget buildIconButton(IconData iconData, int index) {
@@ -143,7 +183,7 @@ class _TemplatePageState extends State<TemplatePage> {
         IconButton(
           icon: Icon(
             iconData,
-            size: isSelected ? 100 : 60,
+            size: isSelected ? 60 : 60,
             color: isSelected ? Colors.black : Colors.grey,
           ),
           onPressed: () {
@@ -153,27 +193,7 @@ class _TemplatePageState extends State<TemplatePage> {
             });
           },
           color: isSelected ? Colors.black : Colors.transparent,
-          iconSize: isSelected ? 100 : 60,
-          padding: EdgeInsets.all(10),
-          splashRadius: 40,
-          splashColor: Colors.grey.withOpacity(0.5),
-          highlightColor: Colors.transparent,
-        ),
-        IconButton(
-          icon: Icon(
-            iconData,
-            size: isSelected ? 100 : 60,
-            color: isSelected ? Colors.black : Colors.grey,
-          ),
-          onPressed: () {
-            setState(() {
-              methodIndex = index;
-
-              updateMethodContent();
-            });
-          },
-          color: isSelected ? Colors.black : Colors.transparent,
-          iconSize: isSelected ? 100 : 60,
+          iconSize: isSelected ? 60 : 60,
           padding: EdgeInsets.all(10),
           splashRadius: 40,
           splashColor: Colors.grey.withOpacity(0.5),
@@ -184,16 +204,12 @@ class _TemplatePageState extends State<TemplatePage> {
   }
 
   void updateMethodContent() {
-    // TODO: content[methodIndex] is the text/audio/video content for method i (methodIndex)
-    // TODO: dynamically update the content here based on the language chosen and the method chosen
     methodContent = Text(
-        languageIndex != -1
-            ? (languages[languageIndex] + " | " + content[methodIndex])
-            : "no content",
-        style: TextStyle(
-          fontSize: 20.0,
-          color: Colors.white,
-        ),
-      );
+      contentDescriptionMap[languages[languageIndex]]![methodIndex],
+      style: TextStyle(
+        fontSize: 20.0,
+        color: Colors.white,
+      )
+    );
   }
 }
