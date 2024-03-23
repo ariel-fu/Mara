29fc1893a7e6eebb7215d9b4c7b25d502cf90942
shrimann23 <shrima.narasimhan@gmail.com>
2024-03-23 16:42:38 -0700

    fixed video asset change based on language

--- mara_app/lib/pattern_page.dart
+++ mara_app/lib/pattern_page.dart
@@ -16,8 +16,8 @@ class _PatternPageState extends State<PatternPage> {
   String videoAsset2 = 'videoAudio/videos/funnyCat2.mp4';
   String videoTitle2 = 'Video 2 Language Not Selected';
 
-  late Widget video1;
-  late Widget video2;
+  Widget video1 = VideoWidget(videoAsset: 'videoAudio/videos/funnyCat.mp4', title:'Video 1 Language Not Selected');
+  Widget video2 = VideoWidget(videoAsset: 'videoAudio/videos/funnyCat2.mp4', title:'Video 2 Language Not Selected');
   
   int methodIndex = -1; // Index of the selected icon button, -1 for none
   int languageIndex = -1; // similar indexing for language
@@ -70,7 +70,7 @@ class _PatternPageState extends State<PatternPage> {
     },
     '1': { // Language code 1
       'video': 'videoAudio/videos/funnyCat.mp4',
-      'text': 'Dhuluo Video #1',
+      'text': 'Dholuo Video #1',
     },
     '2': { // Language code 2
       'video': 'videoAudio/videos/funnyCat2.mp4',
@@ -83,11 +83,11 @@ class _PatternPageState extends State<PatternPage> {
       'text': 'Kiswahili Video #2',
     },
     '1': {
-      'video': 'videoAudio/videos/funnyCat.mp4',
+      'video': 'videoAudio/videos/funnyCat2.mp4',
       'text': 'Dholuo Video #2',
     },
     '2': {
-      'video': 'videoAudio/videos/funnyCat2.mp4',
+      'video': 'videoAudio/videos/chimes.mp4',
       'text': 'English Video #2',
     },
   },
@@ -127,6 +127,8 @@ class _PatternPageState extends State<PatternPage> {
                         setState(() {
                           languageIndex = 0;
                           updateMethodContent();
+                          video1 = updateVideoContent1();
+                          video2 = updateVideoContent2();
                         });
                       },
                       style: ElevatedButton.styleFrom(
@@ -140,6 +142,8 @@ class _PatternPageState extends State<PatternPage> {
                         setState(() {
                           languageIndex = 1;
                           updateMethodContent();
+                          video1 = updateVideoContent1();
+                          video2 = updateVideoContent2();
                         });
                       },
                       style: ElevatedButton.styleFrom(
@@ -153,6 +157,8 @@ class _PatternPageState extends State<PatternPage> {
                         setState(() {
                           languageIndex = 2;
                           updateMethodContent();
+                          video1 = updateVideoContent1();
+                          video2 = updateVideoContent2();
                         });
                       },
                       style: ElevatedButton.styleFrom(
@@ -208,11 +214,13 @@ class _PatternPageState extends State<PatternPage> {
                         
                         children: [
                           Expanded(// Adjust the aspect ratio as needed
-                              child: updateVideoContent1(),
+                              child:video1,
+                              // child: updateVideoContent1(),
                               //VideoWidget(videoAsset: videoAsset1, title: videoTitle1),
                           ),
                           Expanded(
-                              child: updateVideoContent2(),
+                              child:video2,
+                              // child: updateVideoContent2(),
                               // child: VideoWidget(videoAsset: videoAsset2, title: videoTitle2),
                           ),
                         ],
@@ -254,18 +262,6 @@ class _PatternPageState extends State<PatternPage> {
       ],
     );
   }
-
-  // void updateMethodContent() {
-  //   methodContent = Text(
-  //     languageIndex != -1
-  //         ? (languages[languageIndex] + " | " + content[methodIndex])
-  //         : "no content",
-  //     style: TextStyle(
-  //       fontSize: 20.0,
-  //       color: Colors.white,
-  //     ),
-  //   );
-  // }
 
   void updateMethodContent() {
     methodContent = Text(
@@ -313,201 +309,3 @@ class _PatternPageState extends State<PatternPage> {
       return VideoWidget(videoAsset: videoAsset2, title: videoTitle2);
   }
 }
-
-
-// import 'dart:io';
-// import 'package:flutter/material.dart';
-// import 'package:flutter/material.dart' as flutter;
-// import 'package:excel/excel.dart';
-// import 'video.dart';
-// import 'audio.dart';
-// import 'icons/mara_icons_icons.dart' show MaraIcons;
-
-// class PatternPage extends StatefulWidget {
-//   const PatternPage({Key? key}) : super(key: key);
-
-//   @override
-//   State<PatternPage> createState() => _PatternPageState();
-// }
-
-// class _PatternPageState extends State<PatternPage> {
-//   bool overrideIndex = false;
-//   int selectedButtonIndex = 0; // Default value
-
-//   List<List<String>> languages = List.generate(3, (_) => <String>[]);
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     loadTranslations();
-//   }
-
-//   Future<void> loadTranslations() async {
-//     await parseExcelFile('assets/string-resources/dummy.xlsx');
-//   }
-
-//   Future<void> parseExcelFile(String filePath) async {
-//     var bytes = File(filePath).readAsBytesSync();
-//     var excel = Excel.decodeBytes(bytes);
-
-//     List<List<String>> arrays = List.generate(3, (_) => <String>[]);
-
-//     for (var table in excel.tables.keys) {
-//       for (var row in excel.tables[table]!.rows) {
-//         for (int i = 0; i < row.length; i++) {
-//           CellValue? val = row[i]?.value;
-//           if (val != null) {
-//             arrays[i].add(val.toString());
-//           }
-//         }
-//       }
-//     }
-
-//     setState(() {
-//       languages = arrays;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // Receive selectedButtonIndex as a route argument if available
-//     final int? routeArgumentIndex =
-//         ModalRoute.of(context)?.settings.arguments as int?;
-
-//     String methodContent = "METHOD CONTENT";
-    
-//     // Update selectedButtonIndex if a valid value is provided from the route
-//     if (routeArgumentIndex != null &&
-//         routeArgumentIndex >= 0 &&
-//         routeArgumentIndex < languages.length &&
-//         !overrideIndex) {
-//       selectedButtonIndex = routeArgumentIndex;
-//     }
-
-//     // var selectedButtonIndex = input == null ? input : 0;
-//     return Scaffold(
-//       appBar: AppBar(
-//         leading: IconButton(
-//           icon: const Icon(Icons.home),
-//           onPressed: () {
-//             Navigator.of(context).pushNamed('/home', arguments: selectedButtonIndex);
-//           },
-//         ),
-//         title: Text('What will happen to my periods?')
-//       ),
-//       body: Column(
-//         children: [
-//           Container(
-//             alignment: Alignment.topCenter,
-//             child: Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//               children:[
-//                 IconButton(
-//                     // icon:Image.asset('assets/string-resources/method_1.png'),
-//                     icon:Icon(MaraIcons.condom, size: 50),
-//                     onPressed: () {
-//                       print("hello!");
-//                       setState(() {
-//                         methodContent = "Method 1!";
-//                       });
-//                      },
-//                   ),
-               
-//                 IconButton(
-//                     // icon:Image.asset('assets/string-resources/method_2.png'),
-//                     icon:Icon(MaraIcons.female_condom, size:50),
-//                     onPressed: () {
-//                       setState(() {
-//                         methodContent = "Method 2!";
-//                       });
-//                     },
-//                 ),
-                  
-//                IconButton(
-//                     // icon:Image.asset('assets/string-resources/method_3.png'),
-//                     icon:Icon(MaraIcons.birth_control_pills, size:50),
-//                     onPressed: () {
-//                       setState(() {
-//                         methodContent = "Method 3!";
-//                       });
-//                     },
-//                 ),
-                  
-//                IconButton(
-//                     // icon:Image.asset('assets/string-resources/method_4.png'),
-//                     icon:Icon(MaraIcons.syringe, size:50),
-//                     onPressed: () {
-//                       setState(() {
-//                         methodContent = "Method 4!";
-//                       });
-//                     },
-//                 ),
-            
-//                 IconButton(
-//                     icon:Icon(MaraIcons.contraceptive_implant, size:50),
-//                     onPressed: () {
-//                       setState(() {
-//                         methodContent = "Method 5!";
-//                       });
-//                     },
-//                   ),
-                
-                  
-//                 IconButton(
-//                     icon:Icon(MaraIcons.iud, size:50),
-//                     onPressed: () {
-//                       setState(() {
-//                         methodContent = "Method 6!";
-//                       });
-//                     },
-//                   ),
-
-//                   IconButton(
-//                     // icon:Image.asset('assets/string-resources/method_5.png'),
-//                     icon:Icon(MaraIcons.double_pills, size:50),
-//                     onPressed: () {
-//                       setState(() {
-//                         methodContent = "Method 7!";
-//                       });
-//                     },
-//                 ),
-//                 ],
-
-//             ),
-//           ),
-//           // Container(
-//           //   alignment:Alignment.center,
-//           //   child: AudioWidget(audioAsset: 'purr.mp3'),
-//           // ),
-//           Container(
-//             decoration: BoxDecoration(
-//               border: flutter.Border.all(
-//                 color: Colors.black,
-//                 width: 2.0,
-//               ),
-//             ),
-//             alignment:Alignment.bottomCenter,
-//             child: Text(methodContent, style: TextStyle(
-//                     fontSize: 18.0)),
-//           ),
-          
-//           Expanded (
-//             child: Row(
-//               children: [
-//                 Expanded(
-//                   child: VideoWidget(videoAsset: "videoAudio/videos/chimes.mp4", title: "Wind Chimes"),
-//                 ),
-//                 Expanded(
-//                   child: VideoWidget(videoAsset: "videoAudio/videos/funnyCat.mp4", title: "Funny Cat"),
-//                 ),
-//                 Expanded(
-//                   child: VideoWidget(videoAsset: "videoAudio/videos/funnyCat2.mp4", title: "Cool Cat"),
-//                 ),
-//               ]
-//           ),
-//         ),
-//         ]
-//       )
-//     );
-//   }
-// }
