c43004bffafbc4aa7ddcbbc78c76403997c108b8
shrimann23 <shrima.narasimhan@gmail.com>
2024-03-23 15:28:46 -0700

    Added video content to template

--- mara_app/lib/template.dart
+++ mara_app/lib/template.dart
@@ -10,9 +10,16 @@ class TemplatePage extends StatefulWidget {
 
 class _TemplatePageState extends State<TemplatePage> {
   Widget methodContent = Text('DUMMY');
-  int methodIndex = 0; // Index of the selected icon button, 0 for default
-  int languageIndex = 0; // similar indexing for language
+
+  String videoAsset1 = 'videoAudio/videos/funnyCat.mp4';
+  String videoTitle1 = 'Video 1 Language Not Selected';
+  String videoAsset2 = 'videoAudio/videos/funnyCat2.mp4';
+  String videoTitle2 = 'Video 2 Language Not Selected';
+
+  int methodIndex = -1; // Index of the selected icon button, -1 for none
+  int languageIndex = -1; // similar indexing for language
   final languages = ["Kiswahili", "Dholuo", "English"];
+
   bool overrideIndex = false;
   final Map<String, List<String>> contentDescriptionMap = {
     "Kiswahili": [
@@ -43,6 +50,47 @@ class _TemplatePageState extends State<TemplatePage> {
       "method 7 in English"
     ],
   };
+
+  final content = [
+    "method 1",
+    "method 2",
+    "method 3",
+    "method 4",
+    "method 5",
+    "method 6"
+  ];
+
+  //Video HashMap: specifies video asset and text/title based on language and video
+  final Map<String, Map<String, Map<String, String>>> languageToVideo = {
+  'video1': {
+    '0': { // Language code 0
+      'video': 'videoAudio/videos/chimes.mp4',
+      'text': 'Kiswahili Video #1',
+    },
+    '1': { // Language code 1
+      'video': 'videoAudio/videos/funnyCat.mp4',
+      'text': 'Dhuluo Video #1',
+    },
+    '2': { // Language code 2
+      'video': 'videoAudio/videos/funnyCat2.mp4',
+      'text': 'English Video #1',
+    },
+  },
+  'video2': {
+    '0': {
+      'video': 'videoAudio/videos/chimes.mp4',
+      'text': 'Kiswahili Video #2',
+    },
+    '1': {
+      'video': 'videoAudio/videos/funnyCat.mp4',
+      'text': 'Dholuo Video #2',
+    },
+    '2': {
+      'video': 'videoAudio/videos/funnyCat2.mp4',
+      'text': 'English Video #2',
+    },
+  },
+};
 
   @override
   Widget build(BuildContext context) {
@@ -227,8 +275,12 @@ class _TemplatePageState extends State<TemplatePage> {
     );
   }
 
+  String _getAsset(String videoKey, String language) {
+      return languageToVideo[videoKey]?[language]?['video'] ?? 'Asset not found';
+  }
+
   String _getTitle(String videoKey, String language) {
-      return (languageToVideo[videoKey]?[language] ?? 'text').toString();
+    return languageToVideo[videoKey]?[language]?['text'] ?? 'Text not found';
   }
 
   void updateVideoContent() {
