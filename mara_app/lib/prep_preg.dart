ef6bbc75bf4717bc3b46357b15dd46782f0f16b4
wcm-khushi <65826614+wcm-khushi@users.noreply.github.com>
2024-03-30 12:48:02 -0700

    change ui of language buttons in prep preg screen

--- mara_app/lib/prep_preg.dart
+++ mara_app/lib/prep_preg.dart
@@ -130,21 +130,17 @@ class _PrepPageState extends State<PrepPage> {
       ),
     );
   }
-
-  Widget languageButton(String language) {
-    bool isSelected = _currentLanguage == language;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: OutlinedButton(
-        onPressed: () => _changeLanguage(language),
-        child: Text(language),
-        style: OutlinedButton.styleFrom(
-          backgroundColor: isSelected ? Colors.grey : Colors.white,
-          foregroundColor: Colors.black,
-          side: BorderSide(color: isSelected ? Colors.black : Colors.grey),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        ),
+Widget languageButton(String language) {
+  bool isSelected = _currentLanguage == language;
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+    child: ElevatedButton(
+      onPressed: () => _changeLanguage(language),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: isSelected ? Colors.grey : null,
       ),
-    );
-  }
+      child: Text(language),
+    ),
+  );
+}
 }
