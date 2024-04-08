4dd020991f7577ff8ba06a3f8e4b4e4d4a0670ed
Shobhit Srivastava <dicex64@uw.edu>
2024-04-08 14:21:49 -0700

    april 8 updates

--- mara_app/lib/home2.dart
+++ mara_app/lib/home2.dart
@@ -18,16 +18,17 @@ class _MainScreenState extends State<home2> {
   List<bool> _selections = List.generate(6, (_) => false);
   bool get _allSelected => _selections.every((bool selected) => selected);
   void _handleTap(int index) {
+  setState(() { _selections[index] = true; });
   if (index == 0) {
     Navigator.push(
     context,
     MaterialPageRoute(builder: (context) => OptionsPage()),
-  ).then((_) { setState(() { _selections[index] = true; }); });
+  );
   } else if (index == 1) {
     Navigator.push(
     context,
     MaterialPageRoute(builder: (context) => PatternPage()),
-  ).then((_) { setState(() { _selections[index] = true; }); });
+  );
   } else if (index == 2) {
     Navigator.push(
     context,
@@ -58,7 +59,7 @@ class _MainScreenState extends State<home2> {
               //do something
             },
             child: Text(
-              'English',
+              'Kiswahili',
             ),
           ),
           const SizedBox(width: 20),
@@ -67,7 +68,7 @@ class _MainScreenState extends State<home2> {
               //do something else
             },
             child: Text(
-              'Swahili',
+              'Dholuo',
             ),
           ),
           const SizedBox(width: 20),
@@ -76,7 +77,7 @@ class _MainScreenState extends State<home2> {
               //different still
             },      
             child: Text(
-              'Guo',
+              'English',
             ),
           ),
           Spacer(),
@@ -89,36 +90,42 @@ class _MainScreenState extends State<home2> {
               leading: Icon(Icons.info_outline),
               title: Text('What are my options?'),
               onTap: () => _handleTap(0), 
+              tileColor: _selections[0] ? Colors.green : null, 
             ),
             Divider(),
             ListTile(
               leading: Icon(Icons.calendar_today),
               title: Text('What will happen to my period?'),
               onTap: () => _handleTap(1),
+              tileColor: _selections[1] ? Colors.green : null, 
             ),
             Divider(),
             ListTile(
               leading: Icon(Icons.hourglass_empty),
-              title: Text('How long will it last?'),
+              title: Text('How long does the method work?'),
               onTap: () => _handleTap(2),
+              tileColor: _selections[2] ? Colors.green : null, 
             ),
             Divider(),
             ListTile(
               leading: Icon(Icons.pregnant_woman),
               title: Text('What is my chance of getting pregnant?'),
               onTap: () => _handleTap(3),
+              tileColor: _selections[3] ? Colors.green : null, 
             ),
             Divider(),
             ListTile(
-              leading: Icon(Icons.lock_outline),
+              leading: Icon(Icons.lock_outline), 
               title: Text('Can I keep it private?'),
               onTap: () => _handleTap(4),
+              tileColor: _selections[4] ? Colors.green : null, 
             ),
             Divider(),
             ListTile(
               leading: Icon(Icons.baby_changing_station),
               title: Text('What if I’m ready to have a baby?'),
               onTap: () => _handleTap(5),
+              tileColor: _selections[5] ? Colors.green : null, 
             ),
             Divider(),  
             ListTile(
