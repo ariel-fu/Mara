051b9e852f4096c1f3bd8cf0cc8d412c78468308
shrimann23 <71728365+shrimann23@users.noreply.github.com>
2024-04-10 08:56:15 -0700

    Update private_page.dart
    Added new videos

--- mara_app/lib/private_page.dart
+++ mara_app/lib/private_page.dart
@@ -18,31 +18,31 @@ class _PrivatePageState extends State<PrivatePage> {
 
   final Map<String, List<String>> videoContentMap = {
     "Kiswahili": [
-      "videoAudio/videos/funnyCat.mp4", // method 1 - condom
-      "videoAudio/videos/funnyCat.mp4", // method 2 - female condom
-      "videoAudio/videos/funnyCat.mp4", // method 3 - birth control pills
-      "videoAudio/videos/funnyCat.mp4", // method 4 - syringe
-      "videoAudio/videos/funnyCat.mp4", // method 5 - contraceptive implant
-      "videoAudio/videos/funnyCat.mp4", // method 6 - iud
-      "videoAudio/videos/funnyCat.mp4"  // method 7 - double pills
+      "videoAudio/videos/peer/peer3KS.mp4", // method 1 - condom
+      "videoAudio/videos/peer/peer3KS.mp4",
+      "videoAudio/videos/peer/peer3KS.mp4",
+      "videoAudio/videos/peer/peer3KS.mp4",
+      "videoAudio/videos/peer/peer3KS.mp4",
+      "videoAudio/videos/peer/peer3KS.mp4",
+      "videoAudio/videos/peer/peer3KS.mp4",
     ],
     "Dholuo": [
-      "videoAudio/videos/chimes.mp4", // method 1 - condom
-      "videoAudio/videos/chimes.mp4", // method 2 - female condom
-      "videoAudio/videos/chimes.mp4", // method 3 - birth control pills
-      "videoAudio/videos/chimes.mp4", // method 4 - syringe
-      "videoAudio/videos/chimes.mp4", // method 5 - contraceptive implant
-      "videoAudio/videos/chimes.mp4", // method 6 - iud
-      "videoAudio/videos/chimes.mp4"  // method 7 - double pills
+      "videoAudio/videos/peer/peer3DL.mp4",
+      "videoAudio/videos/peer/peer3DL.mp4",
+      "videoAudio/videos/peer/peer3DL.mp4",
+      "videoAudio/videos/peer/peer3DL.mp4",
+      "videoAudio/videos/peer/peer3DL.mp4",
+      "videoAudio/videos/peer/peer3DL.mp4",
+      "videoAudio/videos/peer/peer3DL.mp4",
     ],
     "English": [
-      "videoAudio/videos/funnyCat2.mp4", // method 1 - condom
-      "videoAudio/videos/funnyCat2.mp4", // method 2 - female condom
-      "videoAudio/videos/funnyCat2.mp4", // method 3 - birth control pills
-      "videoAudio/videos/funnyCat2.mp4", // method 4 - syringe
-      "videoAudio/videos/funnyCat2.mp4", // method 5 - contraceptive implant
-      "videoAudio/videos/funnyCat2.mp4", // method 6 - iud
-      "videoAudio/videos/funnyCat2.mp4"  // method 7 - double pills
+      "videoAudio/videos/peer/peer3E.mp4",
+      "videoAudio/videos/peer/peer3E.mp4",
+      "videoAudio/videos/peer/peer3E.mp4",
+      "videoAudio/videos/peer/peer3E.mp4",
+      "videoAudio/videos/peer/peer3E.mp4",
+      "videoAudio/videos/peer/peer3E.mp4",
+      "videoAudio/videos/peer/peer3E.mp4",
     ],
   };
 
@@ -276,10 +276,9 @@ class _PrivatePageState extends State<PrivatePage> {
                     //   child: updateMethodContent(),
                     // ),
                   ),
-                  SizedBox(height: 20.0),
-                  Container(
+                  SizedBox(
                     width: boxWidth,
-                    height: availableHeight * 0.6 * 0.5 - 6.5,
+                    height: availableHeight * 0.6 * 0.5,
                     child: Center(
                       child: getVideoContent(),
                     ),
