ff2b02631b5c02c8c0e7a6ff6dfb03f966b8e7ba
Ariel Fu <arielfu@uw.edu>
2024-04-11 12:58:11 -0400

    Update private_page.dart

--- mara_app/lib/private_page.dart
+++ mara_app/lib/private_page.dart
@@ -243,14 +243,12 @@ class _PrivatePageState extends State<PrivatePage> {
               ),
             ),
           ),
-
           SizedBox(height: 15.0),
           Flex(
-            crossAxisAlignment: CrossAxisAlignment.center,
             direction: Axis.vertical,
             children: [
               Container(
-                  width: boxWidth,
+                  width: boxWidth * 0.9,
                   height: boxHeight*0.2,
                   decoration: BoxDecoration(
                     color: Colors.grey.shade200,
@@ -262,8 +260,8 @@ class _PrivatePageState extends State<PrivatePage> {
                         Icon(Icons.lightbulb_outline, color: Colors.amber),
                         Center(
                             child: SizedBox(
-                                width: boxWidth * 0.9,
-                                height: availableHeight * 0.5*0.3,
+                                width: boxWidth * 0.75,
+                                height: availableHeight * 0.5 * 0.3,
                                 child: Center(
                                   child: updateMethodContent(),
                                 )
@@ -271,24 +269,17 @@ class _PrivatePageState extends State<PrivatePage> {
                         )
                       ]
                   )
-                // child: Center(
-                //   child: updateMethodContent(),
-                // ),
               ),
-              SizedBox(height: 15.0),
-              SizedBox(
-                width: boxWidth*0.8,
-                height: availableHeight * 0.6 * 0.5,
-                child: Center(
-                  child: getVideoContent(),
-                ),
-              ),
-            ],
+            ]
           ),
-          // SizedBox(
-          //   height: availableHeight * 0.70, // Adjust as needed
-          //       child:
-          //   ),
+          SizedBox(height: 15.0),
+          SizedBox(
+            width: boxWidth*0.8,
+            height: availableHeight * 0.6 * 0.5,
+            child: Center(
+              child: getVideoContent(),
+            ),
+          )
         ],
       ),
     );
