1ebd0546851208a5c880dbfd348393c917fb1153
wcm-khushi <65826614+wcm-khushi@users.noreply.github.com>
2024-04-19 11:20:59 -0700

    updated quiz screen

--- mara_app/lib/quiz_screen.dart
+++ mara_app/lib/quiz_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'recommendation_screen.dart';
 import 'recommendation_model.dart';
 
-
 class QuizScreen extends StatefulWidget {
   @override
   _QuizScreenState createState() => _QuizScreenState();
@@ -63,9 +62,10 @@ final Map<String, Map<String, String>> _translations = {
     'option1': 'Could be OK',
     'option2': 'Not OK', 
     'option3': 'I\'m Not sure', 
-    'op1': 'In More than 1 year', 
+    'op1': 'More than 1 year', 
     'op2': 'In 6-12 months', 
     'op3': 'I\'m not sure', 
+    'op4': 'Now or very soon', 
     // ... other English translations
     'o1': 'Extremely important', 
     'o2': 'Somewhat important', 
@@ -130,6 +130,7 @@ final Map<String, Map<String, String>> _translations = {
     'op1': 'Katika zaidi ya mwaka 1', 
     'op2': 'Katika miezi 6-12', 
     'op3': 'Sina uhakika', 
+    'op4': 'Sasa au hivi karibuni', 
     'o1': 'Muhimu sana', 
     'o2': 'Muhimu kwa kiasi fulani', 
     'o3': 'Sio Muhimu', 
@@ -194,6 +195,7 @@ final Map<String, Map<String, String>> _translations = {
     'op1': 'Mokalo higa 1', 
     'op2': 'Ekind dweche 6-12', 
     'op3': 'Ok an gi adiera', 
+    'op4': 'Sani kata machiegni', 
     'o1': 'Omokona ahinya', 
     'o2': 'Omokona marach rach',
     'o3': 'Ok omokona', 
@@ -448,20 +450,27 @@ void _submitQuiz() {
     case 'op2': // In 6-12 months
       recommendations.addAll([
           'Pills, Condoms, Implant, IUCD',
-          // 'Start a prenatal vitamin for good pregnancy health.'
+
         ]);
       //recommendations.addAll(RecommendationModel.getRecommendationsBasedOnPregnancyTiming('In 6-12 months'));
       // introTexts.add('Based on when you think you want a pregnancy, the following methods may be a good choice for you:');
       introTexts.add('introText4');
        outroTexts.addAll(RecommendationModel.getOutroTextsBasedOnPregnancyTiming('In 6-12 months'));
       break;
-    case 'op3': // Now or very soon
+    case 'op3': //'I\'m Not sure'
       recommendations.addAll([
-          'No method, Pills, Condoms',
-          // 'Start a prenatal vitamin for good pregnancy health.'
+          'Condoms, Emergency', 
+
         ]);
-      //recommendations.addAll(RecommendationModel.getRecommendationsBasedOnPregnancyTiming('Not sure'));
-      // introTexts.add('Based on when you think you want a pregnancy, the following methods may be a good choice for you:');
+      
+      introTexts.add('introText4');
+      outroTexts.addAll(RecommendationModel.getOutroTextsBasedOnPregnancyTiming('I\'m not sure'));
+    case 'op4': //'Now or very soon'
+      recommendations.addAll([
+          'Pills, Condoms', 
+
+        ]);
+      
       introTexts.add('introText4');
       outroTexts.addAll(RecommendationModel.getOutroTextsBasedOnPregnancyTiming('Now or very soon'));
       break;
@@ -681,7 +690,6 @@ bool _areAllQuestionsAnswered() {
     return Scaffold(
       appBar: AppBar(
         title: Text(_t('title')),
-        centerTitle: true,
       ),
       body: Column(
         children: <Widget>[
@@ -700,7 +708,7 @@ bool _areAllQuestionsAnswered() {
                 subQuestionSection(context, 'subq1', ['option1', 'option2', 'option3']),
                 subQuestionSection(context, 'subq2', ['option1', 'option2', 'option3']),
                 subQuestionSection(context, 'subq3', ['option1', 'option2', 'option3']),
-                quizSection(context, 'q2', ['op1', 'op2', 'op3']),
+                quizSection(context, 'q2', ['op1', 'op2', 'op3', 'op4']),
                 quizSection(context, 'q3', []),
                 subQuestionSection(context, 'q3sub1', ['option1', 'option2', 'option3']),
                 subQuestionSection(context, 'q3sub2', ['option1', 'option2', 'option3']),
@@ -785,8 +793,6 @@ Widget subQuestionSection(BuildContext context, String questionKey, List<String>
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            ImageIcon(AssetImage('assets/misc-icons/question_circle.png'), color: Colors.black),
-            // if we can make the icon NEXT TO the question text, that would be nice. For now, this is OK.
             Text(
               _t(questionKey),
               style: TextStyle(
