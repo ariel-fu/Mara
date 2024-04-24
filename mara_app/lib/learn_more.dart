d7c93bc5ff557df5b89be98388cd4d96d7f27aae
lisaorii <lisa.orii@gmail.com>
2024-04-24 11:05:19 +0300

    update female condom efficacy

--- mara_app/lib/learn_more.dart
+++ mara_app/lib/learn_more.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mara_app/icons/mara_icons_icons.dart';
 import 'video.dart';
+import 'package:mara_app/audio.dart';
 
 class LearnMoreFertility extends StatefulWidget {
   const LearnMoreFertility({Key? key}) : super(key: key);
@@ -16,27 +17,61 @@ class _LearnMoreFertilityState extends State<LearnMoreFertility> {
   final languages = ["Kiswahili", "Dholuo", "English"];
   bool overrideIndex = false; // Used to override language selection from route
 
+  final Map<String, List<String>> audioContentMap = {
+    "English": [
+      'videoAudio/audio/what_if_condom_E.mp3',
+      'videoAudio/audio/what_if_condom_E.mp3',
+      'videoAudio/audio/what_if_pills_E.mp3',
+      'videoAudio/audio/what_if_depo_E.mp3',
+      'videoAudio/audio/what_if_implant_E.mp3',
+      'videoAudio/audio/what_if_iucd_E.mp3',
+      'videoAudio/audio/what_if_epill_E.mp3',
+    ],
+    "Kiswahili": [
+      'videoAudio/audio/what_if_condom_K.mp3',
+      'videoAudio/audio/what_if_condom_K.mp3',
+      'videoAudio/audio/what_if_pills_K.mp3',
+      'videoAudio/audio/what_if_depo_K.mp3',
+      'videoAudio/audio/what_if_implant_K.mp3',
+      'videoAudio/audio/what_if_iucd_K.mp3',
+      'videoAudio/audio/what_if_epill_K.mp3',
+    ],
+    "Dholuo": [
+      'videoAudio/audio/what_if_condom_L.mp3',
+      'videoAudio/audio/what_if_condom_L.mp3',
+      'videoAudio/audio/what_if_pills_L.mp3',
+      'videoAudio/audio/what_if_depo_L.mp3',
+      'videoAudio/audio/what_if_implant_L.mp3',
+      'videoAudio/audio/what_if_iucd_L.mp3',
+      'videoAudio/audio/what_if_epill_L.mp3',
+    ],
+  };
+  // final Map<String, String> audioContentMap = {
+  //   "English": 'videoAudio/audio/what_if_E.mp3',
+  //   "Kiswahili": 'videoAudio/audio/what_if_K.mp3',
+  //   "Dholuo": 'videoAudio/audio/what_if_L.mp3'
+  // };
   final Map<String, String> subtitleTranslations = {
-  "English": "Tap each method to learn more about its fertility consideration.",
-  "Kiswahili": "Gusa kila mbinu ili upate maelezo zaidi kuhusu masuala yake ya uzazi",
-  "Dholuo": "Mul e yor komo nyuol ka yor komo nyuol mondo ipuonjri mang'eny ewi gigo monego ing'e ewi nyaloni mar mako ich"
-};
-final Map<String, String> titleTranslations = {
-  "English": "What if I'm ready to have a baby?", 
-  "Kiswahili": "Je, itakuaje ikiwa niko tayari kupata mtoto?", 
-  "Dholuo": "To ka ayikora mar mako ich to?"
-};
+    "English": "Tap each method to learn more about its fertility consideration.",
+    "Kiswahili": "Gusa kila njia ili ujifunze zaidi kuhusu mchango wake kwenye uzazi.",
+    "Dholuo": "Tuo wuodi matin to ok nang'o chuny gi tim ber."
+  };
+  final Map<String, String> titleTranslations = {
+    "English": "What if I'm ready to have a baby?",
+    "Kiswahili": "Je, itakuaje ikiwa niko tayari kupata mtoto?",
+    "Dholuo": "To ka ayikora mar mako ich to?"
+  };
 
   final Map<String, List<String>> contentDescriptionMap = {
     "Kiswahili": [
       "Kondomu haina athari kwenye mwili wako zaidi ya kuzuia mbegu za mpenzi wako kuingia ndani ya mwili wako. Ikiwa unatumia kondomu na uko tayari kupata mtoto, unaweza kuacha tu kuzitumia. Kumbuka, hata hivyo, kwamba ukiacha kutumia kondomu huwezi kulindwa dhidi ya Virusi Vya Ukimwi au magonjwa mengine ya zinaa.",
       "Kondomu haina athari kwenye mwili wako zaidi ya kuzuia mbegu za mpenzi wako kuingia ndani ya mwili wako. Ikiwa unatumia kondomu na uko tayari kupata mtoto, unaweza kuacha tu kuzitumia. Kumbuka, hata hivyo, kwamba ukiacha kutumia kondomu huwezi kulindwa dhidi ya Virusi Vya Ukimwi au magonjwa mengine ya zinaa.",
-      "Siku chache baada ya kuacha dawa za kila siku, mwili wako utarudi kwenye kiwango chako cha kawaida cha uzazi. Hata kama hujawahi kupata mimba, au ikiwa unatumia vidonge kwa miaka mingi, haitakuwa vigumu zaidi kupata mimba baadaye kwa kutumia vidonge.", 
-      "Kutumia sindano (depo) sio hatari kwa uwezo wako wa baadaye wa kupata mimba - hata kama hujawahi kupata mimba, au hedhi zako zitakoma. Ikiwa unataka kupata mjamzito, acha kupata sindano na uiruhusu kuzima. Watu wengi hurudi kwenye uwezo wao wa kushika mimba mara tu baada ya kudungwa sindano inayofuata, lakini kwa baadhi ya watu huchukua muda mrefu zaidi, hadi takriban miezi 6. Ikiwa unataka kupata mimba katika miezi 6 ijayo, njia tofauti inaweza kuwa chaguo bora kwako.", 
-      "Siku chache baada ya implant kuondolewa, mwili wako utarudi kwenye kiwango chako cha kawaida cha uzazi. Hii ni kweli ikiwa umewahi kupata ujauzito au kuzaliwa. Kwa hiyo, unapokuwa tayari kwa mimba, unaweza tu kuondoa implant!", 
-      "Unaweza kupata mimba mara moja baada ya kuondolewa kwa IUCD (coil)! Kwa hivyo ni sawa kutumia IUCD hadi utakapotaka kupata ujauzito. IUCD haina homoni ndani yake, hivyo mara tu inapoondolewa, ni vizuri kwenda!", 
-      "E-pill ni ya muda mfupi sana. Hata ukiichukua sana, haitakufanya ushindwe kupata mimba katika siku zijazo. E-pill haidhuru mwili wako kwa njia yoyote, lakini pia haifanyi kazi vizuri kuzuia mimba kwa matumizi ya kawaida.", 
-      
+      "Siku chache baada ya kuacha dawa za kila siku, mwili wako utarudi kwenye kiwango chako cha kawaida cha uzazi. Hata kama hujawahi kupata mimba, au ikiwa unatumia vidonge kwa miaka mingi, haitakuwa vigumu zaidi kupata mimba baadaye kwa kutumia vidonge.",
+      "Kutumia sindano (depo) sio hatari kwa uwezo wako wa baadaye wa kupata mimba - hata kama hujawahi kupata mimba, au hedhi zako zitakoma. Ikiwa unataka kupata mjamzito, acha kupata sindano na uiruhusu kuzima. Watu wengi hurudi kwenye uwezo wao wa kushika mimba mara tu baada ya kudungwa sindano inayofuata, lakini kwa baadhi ya watu huchukua muda mrefu zaidi, hadi takriban miezi 6. Ikiwa unataka kupata mimba katika miezi 6 ijayo, njia tofauti inaweza kuwa chaguo bora kwako.",
+      "Siku chache baada ya implant kuondolewa, mwili wako utarudi kwenye kiwango chako cha kawaida cha uzazi. Hii ni kweli ikiwa umewahi kupata ujauzito au kuzaliwa. Kwa hiyo, unapokuwa tayari kwa mimba, unaweza tu kuondoa implant!",
+      "Unaweza kupata mimba mara moja baada ya kuondolewa kwa IUCD (coil)! Kwa hivyo ni sawa kutumia IUCD hadi utakapotaka kupata ujauzito. IUCD haina homoni ndani yake, hivyo mara tu inapoondolewa, ni vizuri kwenda!",
+      "E-pill ni ya muda mfupi sana. Hata ukiichukua sana, haitakufanya ushindwe kupata mimba katika siku zijazo. E-pill haidhuru mwili wako kwa njia yoyote, lakini pia haifanyi kazi vizuri kuzuia mimba kwa matumizi ya kawaida.",
+
     ],
     "Dholuo": [
       "Rabo yunga onge gi rach moro amora e dendi kopogore gi geng'o pi nyodo mar jaherani donjo ei dendi. Ka itiyo gi rabo yunga kendo iyikori mar yudo nyathi, inyalo mana weyo tiyo kodgi. Ng'e gi pachi ni, ka iweyo tiyo gi rabo yunga ok ibi bedo ni igeng'ori kuom yudo kute mag ayaki kata nyae mamoko. ",
@@ -46,7 +81,7 @@ final Map<String, String> titleTranslations = {
       "Ndalo machuok bang golo IUCD, dendi biro dok kaka ne entie e thuolo mar mako ich. Ma en adier kata bed ni ne pok imako ich kata nyuol. Koro ka iseyikori mar mako ich, inyalo mana dhi mondo ogolni Implant.",
       "Inyalo mako ich mapiyo bang ka igolo IUCD [koil]! Koro en gima ni kare tiyo gi IUCD nyaka kinde ma idwaro make ich. IUCD onge gi homons kuome, koro bang ka isegole, to in mana kare dhi nyime!",
       "E-pill en yath matiyo e kinde matin. Kata ka imuonyo mang'eny, ok obi miyo ibed ni ok inyal mako ich e ndalo mabiro. E-pill ok keth dendi e yo moro amora, to bende ok oti maber e gengo ich ka itiyo kode anuoya.",
-      
+
 
     ],
     "English": [
@@ -57,12 +92,12 @@ final Map<String, String> titleTranslations = {
       "A few days after having the implant removed, your body will return to your normal level of fertility. This it true whether or not you have ever had a pregnancy or a birth. So, when you are ready for a pregnancy, you can just have the implant removed! ",
       "You can get pregnant right away after having an IUCD (coil) removed! So it's OK to use the IUCD until you want to have a pregnancy. The IUCD has no hormones in it, so once it is removed, you are good to go!",
       "The E-pill is very short-acting. Even if you take it a lot, it won't make you less able to get pregnant in the future. The E-pill doesn't damage your body in any way, but also does not work well to prevent pregnancy with regular use. ",
-      
+
     ],
   };
 
 
-@override
+  @override
   Widget build(BuildContext context) {
     final int? routeArgumentIndex = ModalRoute.of(context)?.settings.arguments as int?;
     if (routeArgumentIndex != null &&
@@ -75,8 +110,8 @@ final Map<String, String> titleTranslations = {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          icon: Icon(Icons.arrow_back), 
-          onPressed: () => Navigator.of(context).pop(), 
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
         ),
         title: Text(titleTranslations[languages[languageIndex]] ?? "Title not found"),
       ),
@@ -202,7 +237,13 @@ final Map<String, String> titleTranslations = {
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Icon(Icons.lightbulb_outline, color: Colors.amber, size: 24.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(Icons.lightbulb_outline, color: Colors.amber, size: 24.0),
+                getAudio(),
+              ],
+            ),
             SizedBox(width: 10.0),
             Flexible(
               child: Text(
@@ -221,5 +262,9 @@ final Map<String, String> titleTranslations = {
       contentDescriptionMap[languages[languageIndex]]![methodIndex],
       style: TextStyle(fontSize: 20.0, color: Colors.white),
     );
+  }
+
+  Widget getAudio() {
+    return AudioWidget(audioAsset: audioContentMap[languages[languageIndex]]![methodIndex]);
   }
 }
