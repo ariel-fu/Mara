b85f4898c0b8feb7ca80cf7af64f7f30f931260e
shrimann23 <71728365+shrimann23@users.noreply.github.com>
2024-05-07 12:16:15 -0700

    Create video2.dart
    Created video2.dart for Period page with 2 videos, fixed pause/play button issue

---
+++ mara_app/lib/video2.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'main.dart'; // Import your main.dart file
+import 'package:video_player/video_player.dart';
+
+
+//make video a separate widget that you could easily reference in any other file.
+//video widget would have a asset parameter you would pass in, this would be unique to whatever video you want
+//goal: make it as easy as possible to make many videos without having to re-use code (same goes for audio)
+
+class VideoWidget2 extends StatefulWidget {
+  final String videoAsset;
+  final String title;
+  VideoWidget2({Key? key, required this.videoAsset, required this.title}) : super(key:key);
+  @override
+  State<VideoWidget2> createState() => _VideoWidgetState();
+}
+
+class _VideoWidgetState extends State<VideoWidget2> {
+  late VideoPlayerController ourVideo;
+  late Future<void> _initializeVideoPlayerFuture;
+   bool _isPlaying = false;
+  @override
+  void initState() {
+    super.initState();
+    ourVideo = VideoPlayerController.asset(widget.videoAsset);
+    _initializeVideoPlayerFuture = ourVideo.initialize().catchError((error) {
+      print('Error initializing video player: $error');
+    });
+    ourVideo.addListener(() {
+      setState(() {
+        _isPlaying = ourVideo.value.isPlaying;
+      });
+    });
+  }
+
+  void _videoListener() {
+  if (ourVideo.value.isPlaying != _isPlaying) {
+    setState(() {
+      _isPlaying = ourVideo.value.isPlaying;
+    });
+  }
+}
+
+  @override
+  void dispose() {
+    //releasing resources
+    ourVideo.removeListener(_videoListener);
+    ourVideo.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant VideoWidget2 oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.videoAsset != widget.videoAsset) {
+      ourVideo.pause(); // Pause the current video
+      ourVideo.dispose();
+      ourVideo = VideoPlayerController.asset(widget.videoAsset);
+      _initializeVideoPlayerFuture = ourVideo.initialize();
+      ourVideo.addListener(_videoListener);
+      setState(() {
+        _isPlaying = false; // Reset playing state when changing video
+      });
+    }
+  }
+
+  void _onPressedVideo() {
+    setState(() {
+      if (ourVideo.value.isPlaying) {
+        ourVideo.pause();
+      } else {
+        ourVideo.play();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(child: VideoPlayer(ourVideo)),
+                Text(widget.title, style: TextStyle(
+                  fontSize: 12.0, fontWeight:FontWeight.bold
+                ),),
+                FutureBuilder<void>(
+                  future: _initializeVideoPlayerFuture,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return VideoProgressIndicator(
+                        ourVideo,
+                        allowScrubbing: true,
+                        padding: const EdgeInsets.all(8.0),
+                      );
+                    } else {
+                      return CircularProgressIndicator(); // or any other loading indicator
+                    }
+                  },
+                ),
+                IconButton(
+                  icon: Icon(
+                    _isPlaying ? Icons.pause : Icons.play_arrow,
+                  ),
+                  onPressed: () => _onPressedVideo(),
+                  style: IconButton.styleFrom(
+                  foregroundColor: Colors.lightBlue,
+                  backgroundColor: Colors.grey[300],
+                  ),
+                ),
+              ],
+            )
+          ),
+        ],
+      )
+    );
+  }
+}
