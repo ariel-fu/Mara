5785ae66f1e92aac9fe5778e57a095a521976310
Sandy Cheng <59423538+sandyn-c@users.noreply.github.com>
2024-05-20 21:17:35 -0700

    Poetsen and new colors

---
+++ mara_app/lib/design/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MaraColors {
+  static const purple = Color(0xFF67109D);
+  static const lightPurple = Color(0x6067109D);
+  static const magentaPurple = Color(0xFFb50899);
+}
---
+++ mara_app/lib/icons/image_asset_strings.dart
@@ -0,0 +1,6 @@
+class ImagePaths {
+  static const root = 'assets';
+  static const woman = '$root/woman.png';
+  static const iudWoman = '$root/iud_picture_of_woman.png';
+  //...and others images path
+}
