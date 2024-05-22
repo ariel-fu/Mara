46fad88764937ad44e7b153a593f144756883cd4
lisaorii <42143938+lisaorii@users.noreply.github.com>
2024-05-22 11:27:09 -0700

    update font, font size, color for title

--- mara_app/lib/WhatChance.dart
+++ mara_app/lib/WhatChance.dart
@@ -6,6 +6,7 @@ import 'package:mara_app/icons/mara_icons_icons.dart';
 import 'package:mara_app/time_page.dart';
 import 'package:mara_app/whySomeMethodsBetter.dart';
 import 'audio.dart';
+import 'package:mara_app/design/colors.dart';
 
 class WhatChance extends StatefulWidget {
   const WhatChance({Key? key}) : super(key: key);
@@ -134,8 +135,12 @@ class _WhatChanceState extends State<WhatChance> {
         //   icon: Icon(Icons.arrow_back),
         //   onPressed: () => Navigator.of(context).pop(),
         // ),
-        title: Text(
-            titleTranslations[languages[languageIndex]] ?? "Title not found"),
+        title: Center(
+          child: Text(
+            titleTranslations[languages[languageIndex]] ?? "Title not found",
+            style: TextStyle(fontFamily: 'PoetsenOne', color: MaraColors.purple, fontSize: 36.0)
+          )
+        ),
       ),
       body: SingleChildScrollView(
         child: Column(
