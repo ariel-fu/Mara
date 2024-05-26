d584fa114aa4328a5ee3d0860d0850a765714c69
lisaorii <42143938+lisaorii@users.noreply.github.com>
2024-05-25 18:02:17 -0700

    fix typo

--- mara_app/lib/design/colors.dart
+++ mara_app/lib/design/colors.dart
@@ -4,5 +4,5 @@ class MaraColors {
   static const purple = Color(0xFF67109D);
   static const lightPurple = Color(0x6067109D);
   static const magentaPurple = Color(0xFFb50899);
-  static const lavendar = Color.fromARGB(255, 208, 165, 229);
+  static const lavender = Color.fromARGB(255, 208, 165, 229);
 }
