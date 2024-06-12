cf94a2936c764a00020fbdd446f01b9d9b1b05ec
wcm-khushi <65826614+wcm-khushi@users.noreply.github.com>
2024-06-12 10:48:41 -0700

    update session manager w export data in documents

--- mara_app/lib/session_manager.dart
+++ mara_app/lib/session_manager.dart
@@ -1,54 +1,8 @@
-
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
+
 class SessionManager {
-  // static const String _sessionKey = 'user_sessions';
-
-  // static Future<void> startNewSession() async {
-  //   SharedPreferences prefs = await SharedPreferences.getInstance();
-  //   String newSessionId = DateTime.now().toIso8601String();
-  //   List<String> sessions = prefs.getStringList(_sessionKey) ?? [];
-  //   sessions.add(newSessionId);
-  //   await prefs.setStringList(_sessionKey, sessions);
-  //   await prefs.setString('current_session', newSessionId);
-  // }
-
-  // static Future<void> endCurrentSession() async {
-  //   SharedPreferences prefs = await SharedPreferences.getInstance();
-  //   String? currentSession = prefs.getString('current_session');
-  //   if (currentSession != null) {
-  //     String endTimeKey = '$currentSession-end';
-  //     await prefs.setString(endTimeKey, DateTime.now().toIso8601String());
-  //     await prefs.remove('current_session');
-  //   }
-  // }
-
-  // static Future<void> logScreenEntry(String screenName) async {
-  //   SharedPreferences prefs = await SharedPreferences.getInstance();
-  //   String? currentSession = prefs.getString('current_session');
-  //   if (currentSession != null) {
-  //     String entryTimeKey = '$currentSession-$screenName-entry';
-  //     await prefs.setString(entryTimeKey, DateTime.now().toIso8601String());
-  //   }
-  // }
-
-  // static Future<void> logScreenExit(String screenName) async {
-  //   SharedPreferences prefs = await SharedPreferences.getInstance();
-  //   String? currentSession = prefs.getString('current_session');
-  //   if (currentSession != null) {
-  //     String exitTimeKey = '$currentSession-$screenName-exit';
-  //     String durationKey = '$currentSession-$screenName-duration';
-  //     String? entryTime = prefs.getString('$currentSession-$screenName-entry');
-  //     if (entryTime != null) {
-  //       DateTime entryDateTime = DateTime.parse(entryTime);
-  //       int duration = DateTime.now().difference(entryDateTime).inSeconds;
-  //       await prefs.setString(exitTimeKey, DateTime.now().toIso8601String());
-  //       await prefs.setInt(durationKey, duration);
-  //     }
-  //   }
-  // }
-
   static const String _sessionKey = 'user_sessions';
   static const String _currentSessionKey = 'current_session';
 
@@ -62,15 +16,16 @@ class SessionManager {
     return newSessionId;
   }
 
-  // static Future<void> endCurrentSession() async {
-  //   SharedPreferences prefs = await SharedPreferences.getInstance();
-  //   String? currentSession = prefs.getString(_currentSessionKey);
-  //   if (currentSession != null) {
-  //     String endTimeKey = '$currentSession-end';
-  //     await prefs.setString(endTimeKey, DateTime.now().toIso8601String());
-  //     await prefs.remove(_currentSessionKey);
-  //   }
-  // }
+  static Future<void> endCurrentSession() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? currentSession = prefs.getString(_currentSessionKey);
+    if (currentSession != null) {
+      String endTimeKey = '$currentSession-end';
+      await prefs.setString(endTimeKey, DateTime.now().toIso8601String());
+      await exportData(currentSession); // Automatically export data when the session ends
+      await prefs.remove(_currentSessionKey);
+    }
+  }
 
   static Future<void> logScreenEntry(String screenName) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -108,7 +63,7 @@ class SessionManager {
 
   static Future<void> logVideoStart(String videoName, DateTime startTime) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? currentSession = prefs.getString('current_session');
+    String? currentSession = prefs.getString(_currentSessionKey);
     if (currentSession != null) {
       String startTimeKey = '$currentSession-video-$videoName-start';
       await prefs.setString(startTimeKey, startTime.toIso8601String());
@@ -117,7 +72,7 @@ class SessionManager {
 
   static Future<void> logVideoStop(String videoName, DateTime endTime, int duration) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? currentSession = prefs.getString('current_session');
+    String? currentSession = prefs.getString(_currentSessionKey);
     if (currentSession != null) {
       String endTimeKey = '$currentSession-video-$videoName-end';
       String durationKey = '$currentSession-video-$videoName-duration';
@@ -126,49 +81,31 @@ class SessionManager {
     }
   }
 
-  // static Future<void> logQuizChoice(String screenName, String questionKey, String optionKey, String language) async {
-  //   SharedPreferences prefs = await SharedPreferences.getInstance();
-  //   String? currentSession = prefs.getString('current_session');
-  //   if (currentSession != null) {
-  //     String choiceKey = '$currentSession-$screenName-quiz-$questionKey';
-  //     await prefs.setString(choiceKey, '$optionKey ($language)');
-  //   }
-  // }
-
-  static Future<Map<String, dynamic>> getSessionData(String sessionId) async {
+  static Future<void> exportData(String sessionId) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    Map<String, dynamic> sessionData = {};
-    prefs.getKeys().where((key) => key.startsWith(sessionId)).forEach((key) {
-      sessionData[key] = prefs.get(key);
+    Map<String, dynamic> sessionData = prefs.getKeys().fold<Map<String, dynamic>>({}, (Map<String, dynamic> map, String key) {
+      if (key.startsWith(sessionId)) {
+        map[key] = prefs.get(key);
+      }
+      return map;
     });
-    return sessionData;
-  }
 
-  static Future<void> exportData() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String> sessions = prefs.getStringList(_sessionKey) ?? [];
-    String csvData = "Session ID, Key, Value\n";
-    for (String session in sessions) {
-      Map<String, dynamic> sessionData = await getSessionData(session);
-      sessionData.forEach((key, value) {
-        csvData += "$session, $key, $value\n";
-      });
+    String csvData = sessionData.entries.map((e) => '${e.key},${e.value}').join('\n');
+
+    Directory? directory = await getExternalStorageDirectory();
+    if (directory == null) {
+      throw Exception("Could not access the external storage directory.");
     }
 
-    final directory = await getApplicationDocumentsDirectory();
-    final file = File('${directory.path}/session_data.csv');
+    String path = "${directory.path}/Documents/session_data_$sessionId.csv";
+    
+    // If the Documents directory does not exist, create it
+    Directory documentsDir = Directory("${directory.path}/Documents");
+    if (!documentsDir.existsSync()) {
+      documentsDir.createSync(recursive: true);
+    }
+
+    File file = File(path);
     await file.writeAsString(csvData);
   }
-
-  static Future<void> endCurrentSession() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? currentSession = prefs.getString(_currentSessionKey);
-    if (currentSession != null) {
-      String endTimeKey = '$currentSession-end';
-      await prefs.setString(endTimeKey, DateTime.now().toIso8601String());
-      await exportData(); // Automatically export data when the session ends
-      await prefs.remove(_currentSessionKey);
-    }
-  }
-
 }
