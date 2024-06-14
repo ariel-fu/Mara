19f41ec863048bca3429692da909911f36510c78
lisaorii <lisa.orii@gmail.com>
2024-06-14 10:24:53 -0700

    update translations map, add appbar

--- mara_app/lib/thank_you.dart
+++ mara_app/lib/thank_you.dart
@@ -1,20 +1,104 @@
 import 'package:flutter/material.dart';
 import 'launch_screen2.dart';
 import 'package:mara_app/design/colors.dart';
-class ThankYouScreen extends StatelessWidget {
+import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:mara_app/design/colors.dart';
+
+class ThankYouScreen extends StatefulWidget {
+  const ThankYouScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ThankYouScreen> createState() => _ThankYouScreenState();
+}
+
+class _ThankYouScreenState extends State<ThankYouScreen> {
+  int languageIndex = 2; // similar indexing for language
+  bool overrideIndex = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCurrentLanguage();
+  }
+
+  String _currentLanguage = 'English';
+
+  Future<void> _loadCurrentLanguage() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _currentLanguage = prefs.getString('selectedLanguage') ?? 'English';
+    });
+    if (_currentLanguage.contains('English')) {
+      languageIndex = 2;
+    } else if (_currentLanguage.contains('Dholuo')) {
+      languageIndex = 1;
+    } else {
+      languageIndex = 0;
+    }
+  }
+
+  final double _aspectRatio = 16 / 10;
+  final languages = ["Kiswahili", "Dholuo", "English"];
+
+  final Map<String, Map<String, String>> _translations = {
+    'English': {
+      'thankYou': "Thank you for using Mara Divas app!",
+      'button': "Start New Session",
+    },
+    'Dholuo': {
+      'thankYou': "DL Thank you for using Mara Divas app!",
+      'button': "DL Start New Session",
+    },
+    'Kiswahili': {
+      'thankYou': "KS Thank you for using Mara Divas app!",
+      'button': "KS Start New Session",
+    },
+  };
+
+  String _t(String key) {
+    return _translations[_currentLanguage]?[key] ?? key;
+  }
+
   @override
   Widget build(BuildContext context) {
+    _loadCurrentLanguage();
+    final double containerWidth = MediaQuery.of(context).size.width;
+    final double containerHeight = MediaQuery.of(context).size.height;
+    double boxWidth = containerWidth;
+    double boxHeight = containerHeight;
+    double availableHeight = boxHeight;
+
     return Scaffold(
+      // appBar: AppBar(
+      //   automaticallyImplyLeading: false, // Remove the back button
+      //   // title: Text('Thank You!'),
+      // ),
       appBar: AppBar(
-        automaticallyImplyLeading: false, // Remove the back button
-        title: Text('Thank You!'),
+        automaticallyImplyLeading: false, 
+        centerTitle: true,
+        title: PreferredSize(
+          preferredSize: Size.fromHeight(availableHeight * 0.05),
+          child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                languageButton('Kiswahili', 0),
+                SizedBox(width: 40),
+                languageButton('Dholuo', 1),
+                SizedBox(width: 40),
+                languageButton('English', 2),
+              ],
+            ),
+          ),
+        ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Thank you for using Mara!',
+              _t('thankYou'),
               style: TextStyle(fontSize: 24),
               textAlign: TextAlign.center,
             ),
@@ -27,13 +111,47 @@ class ThankYouScreen extends StatelessWidget {
                 backgroundColor: MaraColors.magentaPurple,
               ),
               child: Text(
-                'Back to Launch Screen',
+                _t('button'),
                 style: TextStyle(color: Colors.white),
               ),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  void _switchLanguage(int language) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String temp;
+    if (language == 0) {
+      temp = 'Kiswahili';
+    } else if (language == 1) {
+      temp = 'Dholuo';
+    } else {
+      temp = 'English';
+    }
+    await prefs.setString('selectedLanguage', temp);
+    setState(() {
+      languageIndex = language;
+    });
+  }
+
+  Widget languageButton(String language, int index) {
+    bool isSelected = languages[languageIndex] == language;
+
+    return ElevatedButton(
+      onPressed: () {
+        _switchLanguage(index);
+        setState(() {
+          languageIndex = index;
+          overrideIndex = true;
+        });
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: isSelected ? Colors.grey : null,
+      ),
+      child: Text(language),
     );
   }
 }
