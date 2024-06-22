92c45a56d1e3cff87f1ed9c5c27586aaa313c3e5
wcm-khushi <65826614+wcm-khushi@users.noreply.github.com>
2024-06-22 15:48:13 -0700

    log choices for quiz

--- mara_app/lib/quiz_screen.dart
+++ mara_app/lib/quiz_screen.dart
@@ -357,12 +357,22 @@ void navigateToRecommendationScreen(BuildContext context, String pregnancyTiming
 //   });
 // }
 
+  // void _handleOptionSelection(String questionKey, String optionKey) {
+  //   setState(() {
+  //     _selectedOptions[questionKey] = optionKey;
+  //   });
+  //   print("Option selected: $questionKey = $optionKey");
+  // }
+
   void _handleOptionSelection(String questionKey, String optionKey) {
-    setState(() {
-      _selectedOptions[questionKey] = optionKey;
-    });
-    print("Option selected: $questionKey = $optionKey");
-  }
+  setState(() {
+    _selectedOptions[questionKey] = optionKey;
+  });
+  print("Option selected: $questionKey = $optionKey");
+  // Log the choice using SessionManager
+  SessionManager.logQuizChoice('QuizPage', questionKey, optionKey, _currentLanguage);
+}
+
 
 
 
